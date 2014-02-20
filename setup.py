4267fc2fadd07d99e8749bf5435f71465b0bd445
Jonas Kalderstam <jonas@kalderstam.se>
2014-02-20 11:02:18 +0100

    default theme with no italic comments
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = 'jkstyles',
+      version = '1',
+      description = 'Some pygment styles for LaTeX primarily.',
+      author = 'Jonas Kalderstam',
+      author_email = 'jonas@kalderstam.se',
+      url = 'https://github.com/spacecowboy/',
+      packages = ['jkstyles'],
+      package_dir = {'jkstyles': 'jkstyles'},
+      install_requires = ['pygments'],
+      entry_points = '''
+        [pygments.styles]
+        jkdefault = jkstyles:JKDefaultStyle
+        '''
+    )
